a4d36971fe63f3efc7cd2b23986bb618e61dbb2e
fcarlini <felipercarlini@gmail.com>
2025-05-30 10:36:51 -0300

    [ADD] currency to payment_group_id withholding line

--- account_payment_group_extend/models/l10n_ar_payment_withholding.py
+++ account_payment_group_extend/models/l10n_ar_payment_withholding.py
@@ -16,6 +16,12 @@ class l10nArPaymentRegisterWithholding(models.Model):
         compute="_compute_account_id",
         string="Cuenta"
     )
+    currency_id = fields.Many2one(
+        comodel_name="res.currency",
+        compute="_compute_currency_id",
+        store=True,
+        related=False
+    )
 
     @api.onchange("tax_id")
     def _compute_account_id(self):
@@ -29,3 +35,13 @@ class l10nArPaymentRegisterWithholding(models.Model):
                     account = rec.tax_id.refund_repartition_line_ids.filtered(lambda x: x.account_id)
                     if account:
                         rec.account_id = account[0].account_id.id
+
+    @api.onchange("payment_group_id", "payment_id")
+    def _compute_currency_id(self):
+        ARS = self.env.ref("base.ARS").id
+        for rec in self:
+            rec.currency_id = False
+            if rec.payment_group_id:
+                rec.currency_id = ARS
+            elif rec.payment_id and rec.payment_id.company_currency_id:
+                rec.currency_id = rec.payment_id.company_currency_id.id
