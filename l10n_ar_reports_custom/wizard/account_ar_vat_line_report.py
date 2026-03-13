35ad02414ed697dfeee8db9a14045800192c1e41
fcarlini <felipercarlini@gmail.com>
2026-03-13 13:42:07 -0300

    [ADD] xlsx reports

--- l10n_ar_reports_custom/wizard/account_ar_vat_line_report.py
+++ l10n_ar_reports_custom/wizard/account_ar_vat_line_report.py
@@ -5,6 +5,20 @@ import io
 import os
 import zipfile
 import base64
+import xlsxwriter
+
+def ws_write(ws, row, col, field, style=None):
+    if style:
+        ws.write(row, col, field, style)
+    else:
+        ws.write(row, col, field)
+    if not hasattr(ws, "_col_widths"):
+        ws._col_widths = {}
+    width = len(str(field)) + 2
+    if col not in ws._col_widths or width > ws._col_widths[col]:
+        ws._col_widths[col] = width
+        ws.set_column(col, col, width)
+
 
 class AccountArVatLine(models.TransientModel):
     _name = "account.ar.vat.line.report"
@@ -62,6 +76,21 @@ class AccountArVatLine(models.TransientModel):
         self.generate_reports(reports)
         return self._return_report()
 
+    def action_report_xlsx(self):
+        self.validate_fields()
+        if self.report_type == "purchase":
+            reports = [
+                {'name': 'purchases.xls', 'script': 'purchases_vouchers.sql'},
+                {'name': 'purchases_aliquots.xls', 'script': 'purchases_aliquots.sql'}
+            ]
+        if self.report_type == "sale":
+            reports = [
+                {'name': 'sales.xls', 'script': 'sales_vouchers.sql'},
+                {'name': 'sales_aliquots.xls', 'script': 'sales_aliquots.sql'}
+            ]
+        self.generate_reports_xlsx(reports)
+        return self._return_report()
+
     def validate_fields(self):
         if self.start_date and self.end_date:
             if self.end_date < self.start_date:
@@ -75,6 +104,76 @@ class AccountArVatLine(models.TransientModel):
             for report in reports:
                 zf.writestr(report['name'], self._get_report_content(report['script']))
         self.file_data = base64.b64encode(stream.getvalue())
+
+    def generate_reports_xlsx(self, reports):
+        stream = io.BytesIO()
+        with zipfile.ZipFile(stream, 'w', compression=zipfile.ZIP_DEFLATED) as zf:
+            for report in reports:
+                excel_buffer = self._get_xlsx_buffer(report)
+                zf.writestr(report['name'], excel_buffer.getvalue())
+        self.file_data = base64.b64encode(stream.getvalue())
+
+    def _get_report_xlsx_cols(self, report):
+        if report["script"] == 'purchases_vouchers.sql':
+            cols_names = [
+                "Fecha", "Tipo", "PV", "Nro Comp", "Despacho", "Cod.", "CUIT",
+                "Nom.Apell", "Total", "No gravado", "Excento", "Perc.IVA", "Perc.Nac",
+                "Perc.IIBB", "Perc.Muni", "Imp.Int", "Mon.", "TC", "Cant.Ali", "Cod.Op",
+                "Cred.Comp", "Otros", "CUIT.Emi", "Denom.Emi", "IVA.Com"
+            ]
+            cols = [8,3,5,20,16,2,20,30,15,15,15,15,15,15,15,15,3,10,1,1,15,15,11,30,15]
+        if report["script"] == "purchases_aliquots.sql":
+            cols_names = [
+                "Tipo", "PV", "Nro Comp", "Cod.", "CUIT", "Imp.Neto", "Alic.IVA", "Imp.Liq"
+            ]
+            cols = [3,5,20,2,20,15,4,15]
+        if report["script"] == "sales_vouchers.sql":
+            cols_names = [
+                "Fecha", "Tipo", "PV", "Nro Comp", "Nro Comp Hasta", "Cod.", "CUIT",
+                "Nom.Apell", "Total", "No gravado", "Perc.No.Categ", "Excento", "Perc.Nac",
+                "Perc.IIBB", "Perc.Muni", "Imp.Int", "Mon", "TC", "Cant.Ali", "Cod.Op",
+                "Otros", "Fecha.Vto"
+            ]
+            cols = [8,3,5,20,20,2,20,30,15,15,15,15,15,15,15,15,3,10,1,1,15,8]
+        if report["script"] == "sales_aliquots.sql":
+            cols_names = [
+                "Tipo", "PV", "Nro Comp", "Imp.Neto.Grav", "Alic.IVA", "Imp.Liq"
+            ]
+            cols = [3,5,20,15,4,15]
+        return cols_names, cols
+
+
+    def _get_xlsx_buffer(self, report):
+        cols_names, cols = self._get_report_xlsx_cols(report)
+        len_cols = len(cols)
+        if len(cols_names) != len_cols:
+            raise ValidationError(
+                "El num de columnas debe coincidir con el numero de nombres de columnas"
+            )
+        excel_buffer = io.BytesIO()
+        workbook = xlsxwriter.Workbook(excel_buffer, {'in_memory': True})
+        ws = workbook.add_worksheet(report["name"])
+        title_style = workbook.add_format({
+            'bold': True,
+            'font_color': 'white',
+            'align': 'center',
+            'bg_color': 'blue',
+            'border': 1
+        })
+        for i in range(len_cols):
+            ws_write(ws, 0, i, cols_names[i], style=title_style)
+        row_num = 1
+        for line in self._get_report_content(report["script"]).decode('ISO-8859-1').split("\r\n"):
+            fields = []
+            pos = 0
+            for size in cols:
+                fields.append(line[pos:pos+size])
+                pos += size
+            for i in range(len_cols):
+                ws_write(ws, row_num, i, fields[i])
+            row_num += 1
+        workbook.close()
+        return excel_buffer
 
     def _get_report_content(self, script_name):
         where_query = self._get_where_query()
